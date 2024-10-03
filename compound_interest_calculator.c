c7ca329717ec573f6cff983b2bd60079bf45fab3
Abraham Kinuthia <abrahamkinuthia193@gmail.com>
2024-10-03 12:46:13 +0300

    completed

--- compound_interest_calculator.c
+++ compound_interest_calculator.c
@@ -1,16 +1,23 @@
 /*
-Program to find compound interest by accepting the principle amount,time and rate values.
-Author:Kinuthia Abraham Ngugi.
-REG NO:CT101/G/22378/24.
-Date:30/9/2024.
+Program to find compound interest by accepting the principal amount, time, and rate values.
+Author: Kinuthia Abraham Ngugi.
+REG NO: CT101/G/22378/24.
+Date: 30/9/2024.
 */
 #include <stdio.h>
-#include <math.h>
+
+float calculate_power(float base, float exponent) {
+    float result = 1.0;
+    for (int i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    return result;
+}
 
 int main() {
     float principal, rate, time, amount, compoundInterest;
 
-    printf("input Principal Amount (P): ");
+    printf("Input Principal Amount (P): ");
     scanf("%f", &principal);
 
     printf("Input Rate of Interest (R) in percentage: ");
@@ -19,7 +26,7 @@ int main() {
     printf("Input Time (T) in years: ");
     scanf("%f", &time);
     
-    amount = principal * pow((1 + rate / 100), time);
+    amount = principal * calculate_power((1 + rate / 100), time);
     compoundInterest = amount - principal;
 
     printf("\nPrincipal Amount (P): %.1f\n", principal);
